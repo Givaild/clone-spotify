42c7ff6f8f0d4be68f8e3a9dedbdefbe458715fc
Givaildo de Andrade <givaildo.oliveira@synvia.com>
2024-01-25 16:02:33 -0300

    fix: playlist component

---
+++ app/components/sidebar/playlist.tsx
@@ -0,0 +1,12 @@
+export function Playlist() {
+    return (
+        <nav className="mt-6 pt-6 border-t border-zinc-800 flex flex-col gap-3">
+        <a href="" className="text-sm text-zinc-500 hover:text-zinc-100">Self control</a>
+        <a href="" className="text-sm text-zinc-500 hover:text-zinc-100">Piseiro</a>
+        <a href="" className="text-sm text-zinc-500 hover:text-zinc-100">Musicas Curtidas</a>
+        <a href="" className="text-sm text-zinc-500 hover:text-zinc-100">Funk </a>
+        <a href="" className="text-sm text-zinc-500 hover:text-zinc-100">Black das antigas</a>
+        
+      </nav>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -2,22 +2,17 @@ import { Headphones, Search, CassetteTape, ChevronLeft, ChevronRight, Play, Shuf
 import Image from "next/image";
 import { Circles } from "./components/sidebar/circles";
 import { IconsSidebar } from "./components/sidebar/iconsSidebar";
+import { Playlist } from "./components/sidebar/playlist";
 
 export default function Home() {
   return (
     <div className="h-screen flex flex-col p-6">
       <div className="flex flex-1">
         <aside className="w-72 bg-zinc-950 p-6">
+          
           <Circles/>
           <IconsSidebar/>
-          <nav className="mt-6 pt-6 border-t border-zinc-800 flex flex-col gap-3">
-            <a href="" className="text-sm text-zinc-500 hover:text-zinc-100">Self control</a>
-            <a href="" className="text-sm text-zinc-500 hover:text-zinc-100">Piseiro</a>
-            <a href="" className="text-sm text-zinc-500 hover:text-zinc-100">Musicas Curtidas</a>
-            <a href="" className="text-sm text-zinc-500 hover:text-zinc-100">Funk </a>
-            <a href="" className="text-sm text-zinc-500 hover:text-zinc-100">Black das antigas</a>
-            
-          </nav>
+          <Playlist/>
           
         </aside>
         <main className="flex-1 p-6">
