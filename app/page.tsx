f48db5843fc789dceebe4956521db58dfb1732b8
Givaildo de Andrade <givaildo.oliveira@synvia.com>
2024-01-25 16:49:56 -0300

    fix: remove import

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import { Circles } from "./components/sidebar/circles";
 import { IconsSidebar } from "./components/sidebar/iconsSidebar";
 import { Playlist } from "./components/sidebar/playlist";
