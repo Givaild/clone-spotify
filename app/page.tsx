8684737d210f5517bc518827b263bd58add173e2
Givaildo de Andrade <givaildo.oliveira@synvia.com>
2024-01-25 15:55:57 -0300

    fix: circles componets

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,13 @@
 import { Headphones, Search, CassetteTape, ChevronLeft, ChevronRight, Play, Shuffle, SkipBack, SkipForward, Repeat, PlaySquare, Mic2, Database, Speaker, Volume1, TabletSmartphone } from "lucide-react";
 import Image from "next/image";
+import { Circles } from "./components/sidebar/circles";
 
 export default function Home() {
   return (
     <div className="h-screen flex flex-col p-6">
       <div className="flex flex-1">
         <aside className="w-72 bg-zinc-950 p-6">
-          <div className="flex items-center gap-2">
-            <div className="w-3 h-3 bg-red-500 rounded-full"/>
-            <div className="w-3 h-3 bg-yellow-500 rounded-full"/>
-            <div className="w-3 h-3 bg-green-500 rounded-full"/>
-          </div>
+          <Circles/>
           <nav className="space-y-5 mt-10">
             <a href="" className="flex items-center gap-3 text-sm font-semibold text-zinc-200">
               <Headphones />
