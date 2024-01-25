be896738cf4c01898ffb2f4184b333042954f1d2
Givaildo de Andrade <givaildo.oliveira@synvia.com>
2024-01-25 16:40:48 -0300

    fix: footer components

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,9 @@ import { Playlist } from "./components/sidebar/playlist";
 import { TopButtons } from "./components/main/topButtons";
 import { Recent } from "./components/main/recent";
 import { MadeFor } from "./components/main/madeFor";
+import { ListenNow } from "./components/footer/listenNow";
+import { Player } from "./components/footer/player";
+import { IconsFooter } from "./components/footer/iconsFooter";
 
 export default function Home() {
   return (
@@ -50,57 +53,11 @@ export default function Home() {
 
       <footer className="bg-zinc-800 border-t border-zinc-700 p-6 flex items-center justify-between">
 
-        <div className=" flex items-center gap-3">
-        <Image src="/album.jpeg" width={56} height={56} alt="capa do disco"/>
+        <ListenNow/>
 
-        <div className="flex flex-col">
-
-          <strong className="font-normal">Don't wanna Sleep</strong>
-          <span className="text-xs text-zinc-400">Swif7</span>
-
-          </div>
-
-        </div>
-
-        <div className="flex flex-col items-center">
-          <div className="flex items-center gap-6">
-
-            <Shuffle size={20} className="text-zinc-200"/>
-            <SkipBack size={20} className="text-zinc-200"/>
-
-            <button className="w-10 h-10 flex items-center justify-center pl-1 rounded-full bg-white text-black">
-              <Play  />
-            </button>
-
-            <SkipForward size={20} className="text-zinc-200"/>
-            <Repeat size={20} className="text-zinc-200"/>
-
-          </div>
-
-          <div className="flex items-center gap-2">
-            <span className="text-zinc-400 text-xs">0:21</span>
-              <div className="h-1 rounded-full w-80 bg-zinc-600">
-                <div className="h-1 rounded-full bg-zinc-200 w-12"></div>
-              </div>
-            <span className="text-zinc-400 text-xs">3:11</span>
-          </div>
-        
-        </div>
+        <Player/>
           
-        <div className="flex items-center gap-3">
-          <PlaySquare size={20}/>
-          <Mic2 size={20}/>
-          <Database size={20}/>
-          <Speaker size={20}/>
-          <div className="flex items-center gap-2">
-
-          <Volume1 size={20}/>
-            <div className="h-1 rounded-full w-20 bg-zinc-600">
-                <div className="h-1 rounded-full bg-zinc-200 w-8"></div>
-            </div>
-          </div>
-          <TabletSmartphone size={20}/>  
-        </div>
+        <IconsFooter/>
 
       </footer>
 
